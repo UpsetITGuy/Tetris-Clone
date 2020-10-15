fcde1f167ef2505d4c16ea25be07b4c3f1f608d1
UpsetITGuy <upsetitguy@gmail.com>
2020-10-15 00:31:05 -0500

    Tetris Day 1

---
+++ JS/app.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () => {
+const grid = document.querySelector('.grid')
+let squares = Array.from(document.querySelectorAll('.grid div'))
+const ScoreDisplay = document.querySelector('#score')
+const StartBtn = document.querySelector('#start-button')
+const width = 10
+
+//The Tetrominoes
+const lTetrominoe = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+]
+
+const zTetrominoe = [
+    [0, width, width+1,width*2+1]
+    [width+1, width+2, width*2, width*2+1]
+    [0, width, width+1,width*2+1]
+    [width+1, width+2, width*2, width*2+1]
+
+]
+
+const tTetrominoe = [
+    [1, width, width+1, width+2]
+    [1, width+1, width+2, width*2+1]
+    [width, width+1, width+2, width*2+1]
+    [1, width, width+1, width*2+1]
+]
+
+const oTetrominoe = [
+    [0, 1, width, width+1]
+    [0, 1, width, width+1]
+    [0, 1, width, width+1]
+    [0, 1, width, width+1]
+]
+
+const iTetrominoe = [
+    [1, width+1, width*2+1, width*3+1]
+    [width, width+1, width+2, width+3]
+    [1, width+1, width*2+1, width*3+1]
+    [width, width+1, width+2, width+3]
+
+]
+
+const theTetrominoes = [lTetrominoe, zTetrominoe, tTetrominoe, oTetrominoe, iTetrominoe]
+
+})
